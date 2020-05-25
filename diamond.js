df0fec7b826ab866e48dc87caa2eb68a1e7c4cdb
KatharinaKraus <62668647+KatharinaKraus@users.noreply.github.com>
2020-05-25 21:52:13 +0200

    Add files via upload

---
+++ diamond.js
@@ -0,0 +1,9 @@
+var letters = ['A', 'B', 'C', 'D', 'E']
+function giveDiamond(letter) {
+    var x = letters.index0f(letter) + 1;
+    for (i=1; i <= (x+(x-1)); i++) {
+        for (i = 1; i <= (x+(x-1)); i++) {
+            print('-');
+        }
+    }
+}

1421892446d3c5f730467978f17e53224be58ef3
KatharinaKraus <62668647+KatharinaKraus@users.noreply.github.com>
2020-05-25 22:34:40 +0200

    Delete diamond.js

--- diamond.js
+++
@@ -1,9 +0,0 @@
-var letters = ['A', 'B', 'C', 'D', 'E']
-function giveDiamond(letter) {
-    var x = letters.index0f(letter) + 1;
-    for (i=1; i <= (x+(x-1)); i++) {
-        for (i = 1; i <= (x+(x-1)); i++) {
-            print('-');
-        }
-    }
-}
